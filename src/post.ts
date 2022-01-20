552217cf27e8aaf9f662515d37a73ee177573000
Ben Greenier <ben@bengreenier.com>
2022-01-19 16:19:08 -0800

    feat(win): support windows

--- src/post.ts
+++ src/post.ts
@@ -1,29 +1,30 @@
-
-import * as core from '@actions/core';
-import * as cache from '@actions/cache';
-import { exec } from '@actions/exec';
+import * as core from "@actions/core";
+import * as cache from "@actions/cache";
+import { exec } from "@actions/exec";
 
 export const showStat = async () => {
-	await exec(`/tmp/sccache/sccache -s`);
+  await exec(`${process.env["RUNNER_TEMP"]}/sccache/sccache -s`);
 };
 
 export const saveCache = async () => {
-	try {
-		const cacheKey    = core.getInput('cache-key');
-		await cache.saveCache([`${process.env.HOME}/.cache/sccache`], `${cacheKey}-${new Date().toISOString()}`);
-	} catch(err) {
-		core.setFailed(err.message);
-	}
+  try {
+    const cacheKey = core.getInput("cache-key");
+    await cache.saveCache(
+      [`${process.env.HOME}/.cache/sccache`],
+      `${cacheKey}-${new Date().toISOString()}`
+    );
+  } catch (err: any) {
+    core.setFailed(err?.message);
+  }
 };
 
 export const run = async () => {
-	try {
-		await showStat();
-		await saveCache();
-	} catch(err) {
-		core.setFailed(err.message);
-	}
+  try {
+    await showStat();
+    await saveCache();
+  } catch (err: any) {
+    core.setFailed(err?.message);
+  }
 };
 
 run();
-
